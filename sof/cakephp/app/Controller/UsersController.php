ec21866c9164660a2be97372798456e6817b4d7c
oscarCastroE <oscar19d@hotmail.es>
2014-10-11 17:30:07 -0600

    Corrección del controlador de user

--- sof/cakephp/app/Controller/UsersController.php
+++ sof/cakephp/app/Controller/UsersController.php
@@ -4,28 +4,27 @@ App::uses('AppController', 'Controller');
 
 
 class UsersController extends AppController {
-/*
-public function beforeFilter() {
-    parent::beforeFilter();
+
+	/*public function beforeFilter() {
+		parent::beforeFilter();
         $this->Auth->allow('add');
     }*/
 
-    public function index(){
-        $this->set('users',$this->User->find('all'));
-
-    }
-
-    /*public function index() {
+	/*public function index() {
         $this->User->recursive = 0;
         $this->set('users', $this->paginate());
     }*/
+	
+    public function index(){
+        $this->set('users',$this->User->find('all'));
+    }
 
     public function view($id = null) {
         $this->User->id = $id;
         if (!$this->User->exists()) {
             throw new NotFoundException(__('Invalid user'));
         }
-        $this->set('user', $this->User->read(null, $id));
+        $this->set('users', $this->User->read(null, $id));
     }
 
     public function add() {
