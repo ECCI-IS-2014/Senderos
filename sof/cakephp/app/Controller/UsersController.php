9d2cd15e9148a2bb58637086492dceadc28c0851
oscarCastroE <oscar19d@hotmail.es>
2014-10-12 12:02:31 -0600

    Mejoras en el controlador de User

--- sof/cakephp/app/Controller/UsersController.php
+++ sof/cakephp/app/Controller/UsersController.php
@@ -2,19 +2,7 @@
 
 App::uses('AppController', 'Controller');
 
-
 class UsersController extends AppController {
-
-	/*public function beforeFilter() {
-		parent::beforeFilter();
-        $this->Auth->allow('add');
-    }*/
-
-	/*public function index() {
-        $this->User->recursive = 0;
-        $this->set('users', $this->paginate());
-    }*/
-	
     public function index(){
         $this->set('users',$this->User->find('all'));
     }
@@ -44,51 +32,29 @@ class UsersController extends AppController {
             throw new NotFoundException(__('Invalid user'));
         }
         if ($this->request->is('post') || $this->request->is('put')) {
-        if ($this->User->save($this->request->data)) {
-        $this->Session->setFlash(__('The user has been saved'));
-        return $this->redirect(array('action' => 'index'));
+			if ($this->User->save($this->request->data)) {
+				$this->Session->setFlash(__('The changes have been saved'));
+				return $this->redirect(array('action' => 'index'));
+			}
+			$this->Session->setFlash(__('The changes could not be saved. Please, try again.'));
         }
-        $this->Session->setFlash(
-        __('The user could not be saved. Please, try again.')
-        );
-        } else {
-        $this->request->data = $this->User->read(null, $id);
-        unset($this->request->data['User']['password']);
+		else {
+			$this->request->data = $this->User->read(null, $id);
+			unset($this->request->data['User']['password']);
         }
     }
 
     public function delete($id = null) {
-    $this->request->onlyAllow('post');
-
-    $this->User->id = $id;
-    if (!$this->User->exists()) {
-    throw new NotFoundException(__('Invalid user'));
+		$this->request->onlyAllow('post');
+		$this->User->id = $id;
+		if (!$this->User->exists()) {
+			throw new NotFoundException(__('Invalid user'));
+		}
+		if ($this->User->delete()) {
+			$this->Session->setFlash(__('User deleted'));
+			return $this->redirect(array('action' => 'index'));
+		}
+		$this->Session->setFlash(__('User was not deleted'));
+		return $this->redirect(array('action' => 'index'));
     }
-    if ($this->User->delete()) {
-    $this->Session->setFlash(__('User deleted'));
-    return $this->redirect(array('action' => 'index'));
-    }
-    $this->Session->setFlash(__('User was not deleted'));
-    return $this->redirect(array('action' => 'index'));
-    }
-
-    public function beforeFilter() {
-        parent::beforeFilter();
-        // Allow users to register and logout.
-        $this->Auth->allow('add', 'logout');
-    }
-
-    public function login() {
-        if ($this->request->is('post')) {
-            if ($this->Auth->login()) {
-                return $this->redirect($this->Auth->redirect());
-            }
-            $this->Session->setFlash(__('Invalid username or password, try again'));
-        }
-    }
-
-    public function logout() {
-        return $this->redirect($this->Auth->logout());
-    }
-
 }
