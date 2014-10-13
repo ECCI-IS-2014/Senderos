ebddc5d377b44f4485486fe01b983a1bfa99f95d
unknown <b23258@105-09.ecci.ucr.ac.cr>
2014-10-13 10:18:47 -0600

    Manejo de excepcion, no permite juegos con el mismo nombre

--- sof/cakephp/app/Model/Product.php
+++ sof/cakephp/app/Model/Product.php
@@ -11,7 +11,9 @@ class Product extends AppModel
 /*The $validate array tells CakePHP how to validate your data when the save() method is called.*/
 	public $validate = array(
         'name' => array(
-            'rule' => 'notEmpty'
+            'rule' => 'notEmpty',
+			'rule' => 'isUnique',
+            'message' => 'The name is already used'
         ),
         'genre' => array(
             'rule' => 'notEmpty'
