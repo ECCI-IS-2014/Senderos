c261ece7b8d3b689c333fe8749472a3a8976bad1
unknown <b06882@105-03.ecci.ucr.ac.cr>
2014-10-15 10:03:38 -0600

    Borrador pruebas unitarias usuario (modelo)

---
+++ sof/cakephp/app/Test/Case/Model/UserTest.php
@@ -0,0 +1,14 @@
+<?php
+App::uses('User', 'Model');
+
+class UserTest extends CakeTestCase {
+    public $fixtures = array('app.user');
+}
+
+public function setUp() {
+        parent::setUp();
+        $this->User = ClassRegistry::init('User');
+    }
+
+
+?>
