18ff4cc7fdc7e497496595a42e6f73f5ee747916
MariaHerreraB <mj_herrera@hotmail.es>
2014-10-18 23:58:22 -0600

    Funcion testBefore save

--- sof/cakephp/app/Test/Case/Model/UserTest.php
+++ sof/cakephp/app/Test/Case/Model/UserTest.php
@@ -3,12 +3,16 @@ App::uses('User', 'Model');
 
 class UserTest extends CakeTestCase {
     public $fixtures = array('app.user');
-}
+
 
 public function setUp() {
         parent::setUp();
         $this->User = ClassRegistry::init('User');
     }
+public function testBeforeSave(){
+		
+	}
+}
 
 
 ?>
