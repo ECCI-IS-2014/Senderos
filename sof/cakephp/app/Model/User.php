1b6ebd7a909e249a7e5d6c941a57177c7cbfad3d
oscarCastroE <oscar19d@hotmail.es>
2014-10-26 10:20:26 -0600

    Correcciones en el modelo USER

--- sof/cakephp/app/Model/User.php
+++ sof/cakephp/app/Model/User.php
@@ -2,58 +2,99 @@
 App::uses('AppModel', 'Model');
 App::uses('BlowfishPasswordHasher', 'Controller/Component/Auth');
 
-class User extends AppModel {
-    public $validate = array(
-        'username' => array(
-			'rule1' => array(
+class User extends AppModel
+{
+    public $validate = array
+    (
+        'username' => array
+        (
+			'rule1' => array
+            (
 				'rule' => array('notEmpty')			
 			),
-			'rule2' => array(
+			'rule2' => array
+            (
 				'rule' => array('isUnique'),
-				'message' => 'The name is already used'			
+				'message' => 'The username is already used'
 			),
-			'rule3' => array(
-				'rule'    => array('minLength', '6'),
-				'message' => 'Minimum 6 characters long'			
-			)			
+            'rule3' => array
+            (
+                'rule' => array('between', 6, 100),
+                'message' => 'Minimum 6 characters long'
+            )
         ),
-		'password' => array(
-			'required' => array(
-				'rule' => array('notEmpty'),
-				'message' => 'A password is required',
-				'rule'    => array('minLength', '8'),
-				'message' => 'Minimum 8 characters long'
-			),
+		'password' => array
+        (
+            'rule1' => array
+            (
+                'rule' => array('notEmpty')
+            ),
+            'rule2' => array
+            (
+                'rule' => array('between', 8, 100),
+                'message' => 'Minimum 8 characters long'
+            )
 		),
-			'name' => array(
-				'required' => array(
-					'rule' => array('notEmpty'),
-					'message' => 'A name is required'
-			)
+		'name' => array
+        (
+            'rule1' => array
+            (
+                'rule' => array('notEmpty')
+            ),
+            'rule2' => array
+            (
+                'rule' => array('between', 2, 100),
+                'message' => 'Minimum 2 characters long'
+            )
+	    ),
+	    'lastname' => array
+        (
+            'rule1' => array
+            (
+                'rule' => array('notEmpty')
+            ),
+            'rule2' => array
+            (
+                'rule' => array('between', 2, 100),
+                'message' => 'Minimum 2 characters long'
+            )
 		),
-			'lastname' => array(
-				'required' => array(
-					'rule' => array('notEmpty'),
-					'message' => 'A lastname is required'
-			)
+        'email' => array
+        (
+            'rule1' => array
+            (
+                'rule' => array('notEmpty')
+            ),
+            'rule2' => array
+            (
+                'rule' => 'email',
+                'message' => 'Invalid email'
+            ),
+            'rule3' => array
+            (
+                'rule' => array('isUnique'),
+                'message' => 'The email is already used'
+            )
+        ),
+		'country' => array
+        (
+            'rule1' => array
+            (
+                'rule' => array('notEmpty')
+            )
 		),
-			'country' => array(
-				'required' => array(
-					'rule' => array('notEmpty'),
-					'message' => 'A country is required'
-			)
-		),
-			'role' => array(
-				'valid' => array(
-					'rule' => array('inList', array('admin', 'cust')),
-					'message' => 'Please enter a valid role',
-					'allowEmpty' => false
-			)
+		'role' => array
+        (
+			'rule' => array('inList', array('admin', 'cust')),
+			'message' => 'Please enter a valid role',
+			'allowEmpty' => false
 		)
     );
 
-    public function beforeSave($options = array()) {
-        if (isset($this->data[$this->alias]['password'])) {
+    public function beforeSave($options = array())
+    {
+        if (isset($this->data[$this->alias]['password']))
+        {
             $passwordHasher = new BlowfishPasswordHasher();
             $this->data[$this->alias]['password'] = $passwordHasher->hash(
                 $this->data[$this->alias]['password']
