a51b2d8a2a124fab85300a91ce254355dc0a4bf0
oscarCastroE <oscar19d@hotmail.es>
2014-10-28 17:39:05 -0600

    Controlador de CATEGORY mejorado

--- sof/cakephp/app/Controller/CategoryController.php
+++ sof/cakephp/app/Controller/CategoryController.php
@@ -6,25 +6,33 @@
  * Time: 01:16 PM
  */
 App::uses('AppController', 'Controller');
-class CategoryController extends AppController {
+class CategoryController extends AppController
+{
     public $helpers = array('Html', 'Form');
     var $components = array('Session');
-    public function index(){
-        $this->set('categories', $this->Category->find('all'));
-        $data = $this->Category->generateTreeList(
+
+    public function index()
+    {
+        $this->set('categories',$this->Category->generateTreeList(
             null,
             null,
             null,
-            '&nbsp;&nbsp;&nbsp;'
-        );
-        debug($data); die;
+            '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'
+        ));
     }
+
     public function add() {
-        //tengo que modificar que no me traiga la categoría actual y que me permita la opción de sin categoría
-        $this->set('categories', $this->Category->find('list'));
-        if ($this->request->is('post')) {
+        $this->set('categories',$this->Category->generateTreeList(
+            null,
+            null,
+            null,
+            '____'
+        ));
+        if ($this->request->is('post'))
+        {
             $this->Category->create();
-            if ($this->Category->save($this->request->data)) {
+            if ($this->Category->save($this->request->data))
+            {
                 $this->Session->setFlash(__('Esta categoría ha sido guardada.'));
                 return $this->redirect(array('action' => 'index'));
             }
