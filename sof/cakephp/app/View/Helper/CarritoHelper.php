f0a6d4a728e650a6da12c023a0e56bd405e52b0f
Blopa <b16195@105-13.ecci.ucr.ac.cr>
2014-10-29 16:04:06 -0600

    Solo falta arreglar 2 lineas del product controller y el calcularCarrito() deberia funcionar :)

--- sof/cakephp/app/View/Helper/CarritoHelper.php
+++ sof/cakephp/app/View/Helper/CarritoHelper.php
@@ -2,13 +2,12 @@
 App::uses('AppHelper','View/Helper');
 
 class CarritoHelper extends AppHelper{
-    public function calcularCarrito($productsInCart,$qty){
+    public function calcularCarrito($productsInCart,$qty,$price){
         $valor = 0;
         $number=0;
         if(isset($productsInCart)){
             foreach ($productsInCart as $productInCart) {
-                //$valor=$valor + $qty.$number;
-
+                $valor=$valor + ( $qty[$number] /* * $price[$number] */);
                 $number++;
             }
         }
