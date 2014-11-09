7672a877e6047ade32bf4b1ca3f15fa3a262baae
oscarCastroE <oscar19d@hotmail.es>
2014-11-09 14:13:18 -0600

    Actualización en el controlador de categorías

--- sof/cakephp/app/Controller/CategoryController.php
+++ sof/cakephp/app/Controller/CategoryController.php
@@ -13,8 +13,9 @@ class CategoryController extends AppController
 
     public function index()
     {
-        //$this->set('data',$this->Category->generateTreeList('all', null, null, ' '));
-        $this->set('data', $this->Category->find('threaded'));
+        $this->data = $this->Category->generateTreeList(null, null, null, '');
+        $this->set('categorylist',$this->data);
+        //$this->set('data', $this->Category->find('threaded', array('order' => array('Category.name' => 'asc'))));
     }
 
     // Función encargada de mostrar la información de cada categoría.
@@ -26,8 +27,11 @@ class CategoryController extends AppController
             throw new NotFoundException(__('Categoría Inválida'));
         }
         $this->set('category', $this->Category->read(null, $id));
+        $parentName = $this->Category->getParentNode($id);
+        $this->set('parent', $parentName);
         $this->set('child', $this->Category->find('list', array(
-                    'conditions' => array('parent_id' => $id))));
+                    'conditions' => array('parent_id' => $id),
+                    'order' => array('Category.name' => 'asc'))));
     }
 
     // Función encargada de agregar categorías a la base de datos.
@@ -78,4 +82,53 @@ class CategoryController extends AppController
             $this->request->data = $this->Category->read(null, $id);
         }
     }
+
+    public function delete($id = null)
+    {
+        $this->request->onlyAllow('post');
+        $this->Category->id = $id;
+        if (!$this->Category->exists()) {
+            throw new NotFoundException(__('Categoría Inválida'));
+        }
+        if ($this->Category->removeFromTree($id, true))
+        {
+            $this->Session->setFlash(__('Categoría Borrada'));
+            return $this->redirect(array('action' => 'index'));
+        }
+        $this->Session->setFlash(__('La categoría no pudo eliminarse'));
+        return $this->redirect(array('action' => 'index'));
+    }
+
+    function moveup($id = null)
+    {
+        $this->Category->id = $id;
+        if (!$this->Category->exists())
+        {
+            throw new NotFoundException(__('Categoría Inexistente'));
+        }
+        if($this->Category->moveUp())
+        {
+            $this->Session->setFlash(__('Categoría Modificada'));
+            return $this->redirect(array('action' => 'index'));
+        }
+        $this->Session->setFlash(__('No pudo hacerse la modificación'));
+        return $this->redirect(array('action' => 'index'));
+    }
+
+    function movedown($id = null)
+    {
+        $this->Category->id = $id;
+        if (!$this->Category->exists())
+        {
+            throw new NotFoundException(__('Categoría Inexistente'));
+        }
+        if($this->Category->moveDown())
+        {
+            $this->Session->setFlash(__('Categoría Modificada'));
+            return $this->redirect(array('action' => 'index'));
+        }
+        $this->Session->setFlash(__('No pudo hacerse la modificación'));
+        return $this->redirect(array('action' => 'index'));
+    }
+
 }
