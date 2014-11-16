b5cae02b0afddf486b07a86a7b19fd4bdb02124c
MariaHerreraB <mj_herrera@hotmail.es>
2014-11-15 21:31:58 -0600

    Pruebas Unitarias de Checks

---
+++ sof/cakephp/app/Test/Case/Controller/CheckControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MariaJose
+ * Date: 13/11/2014
+ * Time: 09:18 PM
+ */
+class CheckControllerTest extends ControllerTestCase
+{
+    public $fixtures = array('app.check','app.checkproduct');
+
+    public function testCheck() {
+        $result = $this->testAction('checks/check');
+        debug($result);
+    }
+}
+?>
