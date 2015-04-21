fbd5b83e7c5abd2cd0a55d274b5ba3ee44e67611
oscarCastroE <oscar19d@hotmail.es>
2015-04-21 03:17:20 -0600

    Script de tablas

--- doc/inge2.sql
+++ doc/inge2.sql
@@ -1,190 +1,183 @@
--- --------------------------------------------------------
+-- CreaciÛn de las tablas
+CREATE TABLE stations
+(
+  id int NOT NULL,
+  name varchar(100) NOT NULL,
+  location varchar(100) NOT NULL,
+  description varchar(100) NOT NULL,
+  PRIMARY KEY(id)
+);
 
---
--- Estructura de tabla para la tabla `countries`
---
+CREATE TABLE trails
+(
+  id int NOT NULL,
+  name varchar(100) NOT NULL,
+  description varchar(100) NOT NULL,
+  map multipoint NOT NULL,
+  station_id int NOT NULL,
+  PRIMARY KEY(id),
+  FOREIGN KEY(station_id) REFERENCES stations(id)
+);
 
-CREATE TABLE IF NOT EXISTS `countries` (
-`id` int(10) NOT NULL,
-  `code` varchar(2) NOT NULL DEFAULT '',
-  `name` varchar(100) NOT NULL DEFAULT ''
-) ENGINE=MyISAM AUTO_INCREMENT=240 DEFAULT CHARSET=utf8;
+CREATE TABLE points 
+(
+  id int NOT NULL,
+  name varchar(100) NOT NULL,
+  cordx point NOT NULL,
+  cordy point NOT NULL,
+  description varchar(100) NOT NULL,
+  trail_id int NOT NULL,
+  PRIMARY KEY(id),
+  FOREIGN KEY(trail_id) REFERENCES trails(id)
+);
 
--- --------------------------------------------------------
+CREATE TABLE multimedias 
+(
+  id int NOT NULL,
+  name varchar(100) NOT NULL,
+  description varchar(500) NOT NULL,
+  type varchar(100) NOT NULL,
+  route varchar(100) NOT NULL,
+  language varchar(100) NOT NULL,
+  point_id int NOT NULL,
+  PRIMARY KEY(id),
+  FOREIGN KEY(point_id) REFERENCES points(id)
+);
 
---
--- Estructura de tabla para la tabla `multimedia`
---
+CREATE TABLE point_multimedias 
+(
+  point_id int NOT NULL,
+  multimedia_id int NOT NULL,
+  PRIMARY KEY(point_id, multimedia_id),
+  FOREIGN KEY(point_id) REFERENCES points(id),
+  FOREIGN KEY(multimedia_id) REFERENCES multimedias(id)
+);
 
-CREATE TABLE IF NOT EXISTS `multimedias` (
-`id` int(10) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `description` varchar(500) NOT NULL,
-  `type` varchar(100) NOT NULL,
-  `route` varchar(100) NOT NULL,
-  `language` varchar(100) NOT NULL,
-  `point_id` int(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE countries
+(
+  id int NOT NULL,
+  code varchar(2) NOT NULL,
+  name varchar(100) NOT NULL,
+  PRIMARY KEY(id)
+);
 
--- --------------------------------------------------------
+CREATE TABLE users
+(
+  id int NOT NULL,
+  username varchar(100) NOT NULL,
+  name varchar(100) NOT NULL,
+  lastname varchar(100) NOT NULL,
+  role varchar(100) NOT NULL,
+  password varchar(100) NOT NULL,
+  country_id int NOT NULL,
+  PRIMARY KEY(id),
+  FOREIGN KEY(country_id) REFERENCES countries(id)
+);
 
---
--- Estructura de tabla para la tabla `point`
---
+CREATE TABLE visitors
+(
+  id int NOT NULL,
+  role varchar(100) NOT NULL,
+  multimedia_id int NOT NULL,
+  PRIMARY KEY(id),
+  FOREIGN KEY(multimedia_id) REFERENCES multimedias(id)
+);
 
-CREATE TABLE IF NOT EXISTS `points` (
-`id` int(10) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `cordx` point NOT NULL,
-  `cordy` point NOT NULL,
-  `description` varchar(100) NOT NULL,
-  `trail_id` int(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+-- Autoincrementos para las tablas.
 
--- --------------------------------------------------------
+CREATE SEQUENCE stations_seq;
+CREATE SEQUENCE trails_seq;
+CREATE SEQUENCE points_seq;
+CREATE SEQUENCE multimedias_seq;
+CREATE SEQUENCE visitors_seq;
+CREATE SEQUENCE country_seq;
+CREATE SEQUENCE users_seq;
 
-CREATE TABLE IF NOT EXISTS `point_multimedias` (
-`point_id` int(10) NOT NULL,
-`multimedia_id` int(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE OR REPLACE TRIGGER stations_ai
+BEFORE INSERT ON stations 
+FOR EACH ROW
+BEGIN
+  SELECT stations_seq.NEXTVAL
+  INTO   :new.id
+  FROM   dual;
+END;
 
--- --------------------------------------------------------
+CREATE OR REPLACE TRIGGER trails_ai
+BEFORE INSERT ON trails
+FOR EACH ROW
+BEGIN
+  SELECT trails_seq.NEXTVAL
+  INTO   :new.id
+  FROM   dual;
+END;
 
---
--- Estructura de tabla para la tabla `station`
---
+CREATE OR REPLACE TRIGGER points_ai
+BEFORE INSERT ON points
+FOR EACH ROW
+BEGIN
+  SELECT points_seq.NEXTVAL
+  INTO   :new.id
+  FROM   dual;
+END;
 
-CREATE TABLE IF NOT EXISTS `stations` (
-`id` int(10) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `location` varchar(100) NOT NULL,
-  `description` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE OR REPLACE TRIGGER multimedias_ai
+BEFORE INSERT ON multimedias
+FOR EACH ROW
+BEGIN
+  SELECT multimedias_seq.NEXTVAL
+  INTO   :new.id
+  FROM   dual;
+END;
 
--- --------------------------------------------------------
+CREATE OR REPLACE TRIGGER visitors_ai
+BEFORE INSERT ON visitors
+FOR EACH ROW
+BEGIN
+  SELECT visitors_seq.NEXTVAL
+  INTO   :new.id
+  FROM   dual;
+END;
 
---
--- Estructura de tabla para la tabla `trail`
---
+CREATE OR REPLACE TRIGGER country_ai
+BEFORE INSERT ON countries
+FOR EACH ROW
+BEGIN
+  SELECT country_seq.NEXTVAL
+  INTO   :new.id
+  FROM   dual;
+END;
 
-CREATE TABLE IF NOT EXISTS `trails` (
-`id` int(10) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `description` varchar(100) NOT NULL,
-  `map` multipoint NOT NULL,
-  `station_id` int(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE OR REPLACE TRIGGER users_ai
+BEFORE INSERT ON users
+FOR EACH ROW
+BEGIN
+  SELECT users_seq.NEXTVAL
+  INTO   :new.id
+  FROM   dual;
+END;
 
--- --------------------------------------------------------
+-- Eliminar todas la tablas creadas.
+DROP TABLE users;
+DROP TABLE countries;
+DROP TABLE visitors;
+DROP TABLE point_multimedias;
+DROP TABLE multimedias;
+DROP TABLE points;
+DROP TABLE trails;
+DROP TABLE stations;
 
---
--- Estructura de tabla para la tabla `user`
---
-
-CREATE TABLE IF NOT EXISTS `users` (
-`id` int(11) NOT NULL,
-  `username` varchar(100) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `lastname` varchar(100) NOT NULL,
-  `role` varchar(100) NOT NULL,
-  `password` varchar(100) NOT NULL,
-  `country_id` int(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `visitors`
---
-
-CREATE TABLE IF NOT EXISTS `visitors` (
-  `id` int(10) NOT NULL,
-  `multimedia_id` int(10) NOT NULL,
-  `role` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- √çndices para tablas volcadas
---
-
---
--- Indices de la tabla `countries`
---
-ALTER TABLE `countries`
- ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `multimedia`
---
-ALTER TABLE `multimedias`
- ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `point`
---
-ALTER TABLE `points`
- ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `id` (`id`);
-
---
--- Indices de la tabla `species`
---
-ALTER TABLE `species`
- ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `station`
---
-ALTER TABLE `stations`
- ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `id` (`id`);
-
---
--- Indices de la tabla `trail`
---
-ALTER TABLE `trails`
- ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `id` (`id`);
-
---
--- Indices de la tabla `user`
---
-ALTER TABLE `users`
- ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `username` (`username`), ADD UNIQUE KEY `id` (`id`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `countries`
---
-ALTER TABLE `countries`
-MODIFY `id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=240;
---
--- AUTO_INCREMENT de la tabla `multimedia`
---
-ALTER TABLE `multimedias`
-MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT de la tabla `point`
---
-ALTER TABLE `points`
-MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT de la tabla `species`
---
-ALTER TABLE `species`
-MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT de la tabla `station`
---
-ALTER TABLE `stations`
-MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT de la tabla `trail`
---
-ALTER TABLE `trails`
-MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT de la tabla `user`
---
-ALTER TABLE `users`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+-- Eliminar los autoincrementos
+DROP SEQUENCE stations_seq;
+DROP TRIGGER stations_ai;
+DROP SEQUENCE trails_seq;
+DROP TRIGGER trails_ai;
+DROP SEQUENCE points_seq;
+DROP TRIGGER points_ai;
+DROP SEQUENCE multimedias_seq;
+DROP TRIGGER multimedias_ai;
+DROP SEQUENCE visitors_seq;
+DROP TRIGGER visitors_ai;
+DROP SEQUENCE country_seq;
+DROP TRIGGER country_ai;
+DROP SEQUENCE users_seq;
+DROP TRIGGER users_ai;
