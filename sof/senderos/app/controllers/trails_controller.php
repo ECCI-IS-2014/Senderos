4f2cfd260ed6c085aeee1fb0718a49486385849f
ivannia-alvarado <mana1893@hotmail.com>
2015-04-23 12:09:53 -0600

    Agrego el cargador de imágenes para mapas.

--- sof/senderos/app/controllers/trails_controller.php
+++ sof/senderos/app/controllers/trails_controller.php
@@ -16,10 +16,39 @@ class TrailsController extends AppController {
 		$this->set('trail', $this->Trail->read(null, $id));
 	}
 
+	/*
 	function add() {
 		if (!empty($this->data)) {
 			$this->Trail->create();
 			if ($this->Trail->save($this->data)) {
+				$this->Session->setFlash(__('The trail has been saved', true));
+				$this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash(__('The trail could not be saved. Please, try again.', true));
+			}
+		}
+		$stations = $this->Trail->Station->find('list');
+		$this->set(compact('stations'));
+	}*/
+	
+	function add() {
+		if (!empty($this->data)) {
+			$this->Trail->create();
+			if ($this->Trail->save($this->data)) {
+				//if($this->request->data['Trail']['archivo']['error'] == 0 &&  $this->request->data['Trail']['archivo']['size'] > 0){
+                debug($this->data);
+                debug($this->data['Trail']);
+                if($this->data['Trail']['archivo']['error'] == 0 &&  $this->data['Trail']['archivo']['size'] > 0){
+                    echo('helo');
+					// Informacion del tipo de archivo subido $this->data['Trail']['archivo']['type']
+					//$destino = WWW_ROOT.'uploads'.DS;
+					$destino = WWW_ROOT.'img'.DS;
+					move_uploaded_file($this->data['Trail']['archivo']['tmp_name'], $destino.$this->data['Trail']['archivo']['name']);
+					$id = $this->data['Trail']['id'];
+					$this->Trail->read(null, $id);
+					$this->Trail->set('image', $this->data['Trail']['archivo']['name']);
+					$this->Trail->save();
+				}
 				$this->Session->setFlash(__('The trail has been saved', true));
 				$this->redirect(array('action' => 'index'));
 			} else {
