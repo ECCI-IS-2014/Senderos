d1bac0ffea8ed7cc94f725c2674f055126822410
namasher <eduardo.ecci@gmail.com>
2015-04-28 16:49:27 -1200

    falta arreglar el tipo cuando se edita un documento y falta arreglar que se desplieque el archivo guardado anteriormente

--- sof/senderos/app/controllers/documents_controller.php
+++ sof/senderos/app/controllers/documents_controller.php
@@ -75,6 +75,41 @@ class DocumentsController extends AppController {
 			$this->redirect(array('action' => 'index'));
 		}
 		if (!empty($this->data)) {
+            $nombre =  $this->data['Document']['archivo']['name'];
+            $terminacion = substr($nombre,strlen($nombre)-3);
+            $destino = WWW_ROOT.'files'.DS;
+            switch($terminacion) {
+                case "doc": $destino = WWW_ROOT.'text'.DS; break;
+                case "txt": $destino = WWW_ROOT.'text'.DS; break;
+                case "odt": $destino = WWW_ROOT.'text'.DS; break;
+                case "mp3": $destino = WWW_ROOT.'sound'.DS; break;
+                case "wav": $destino = WWW_ROOT.'sound'.DS; break;
+                case "avi": $destino = WWW_ROOT.'sound'.DS; break;
+                case "flv": $destino = WWW_ROOT.'video'.DS; break;
+                case "wmv": $destino = WWW_ROOT.'video'.DS; break;
+                case "mov": $destino = WWW_ROOT.'video'.DS; break;
+                case "mp4": $destino = WWW_ROOT.'video'.DS; break;
+                case "jpg": $destino = WWW_ROOT.'images'.DS; break;
+                case "png": $destino = WWW_ROOT.'images'.DS; break;
+                default:  $destino = WWW_ROOT.'files'.DS;
+            }
+            move_uploaded_file($this->data['Document']['archivo']['tmp_name'], $destino.$this->data['Document']['archivo']['name']);
+            $id = $this->data['Document']['id'];
+            $this->Document->read(null, $id);
+            $tipo = $this->data['Document']['archivo']['Document.type'];
+            switch($tipo)
+            {
+                case "0": $tipo = "video"; break;
+
+                case "1": $tipo = "text"; break;
+
+                case "2": $tipo = "img"; break;
+
+                default: $tipo = "sound"; break;
+
+            }
+            $this->Document->set('type', $tipo);
+            $this->Document->set('route', $this->data['Document']['archivo']['name']);
 			if ($this->Document->save($this->data)) {
 				$this->Session->setFlash(__('The document has been saved', true));
 				$this->redirect(array('action' => 'index'));
--- sof/senderos/app/views/documents/edit.ctp
+++ sof/senderos/app/views/documents/edit.ctp
@@ -1,13 +1,14 @@
-<div class="trails form">
-<?php echo $this->Form->create('Trail', array('type' => 'file'));?>
+<div class="documents form">
+<?php echo $this->Form->create('Document', array('type' => 'file'));?>
 	<fieldset>
-		<legend><?php __('Edit Trail'); ?></legend>
+		<legend><?php __('Edit Document'); ?></legend>
 	<?php
         echo $this->Form->input('id', array('type' => 'hidden'));
 		echo $this->Form->input('name');
 		echo $this->Form->input('description');
-        echo $this->Form->input('archivo', array('type' => 'file', 'label'=>'Select a map image:'));
-		echo $this->Form->input('station_id');
+		echo $this->Form->input('Document.type',array('type'=>'select','options'=>array('Video','Text','Image','Sound')));
+        echo $this->Form->input('archivo', array('type' => 'file', 'label'=>'Select a file:'));
+		echo $this->Form->input('language');
 	?>
 	</fieldset>
 <?php echo $this->Form->end(__('Submit', true));?>
@@ -15,10 +16,6 @@
 <div class="actions">
 	<h3><?php __('Actions'); ?></h3>
 	<ul>
-
-		<li><?php echo $this->Html->link(__('Delete', true), array('action' => 'delete', $this->Form->value('Trail.id')), null, sprintf(__('Are you sure you want to delete # %s?', true), $this->Form->value('Trail.id'))); ?></li>
-		<li><?php echo $this->Html->link(__('List Trails', true), array('action' => 'index'));?></li>
-		<li><?php echo $this->Html->link(__('List Stations', true), array('controller' => 'stations', 'action' => 'index')); ?> </li>
-		<li><?php echo $this->Html->link(__('New Station', true), array('controller' => 'stations', 'action' => 'add')); ?> </li>
+        <li><?php echo $this->Html->link(__('List Documents', true), array('action' => 'index'));?></li>
 	</ul>
 </div>
