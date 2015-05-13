440cc3f0065ac396bd52889e5646ac7308467743
namasher <eduardo.ecci@gmail.com>
2015-05-13 11:27:51 -1200

    yio

--- sof/senderos/app/controllers/documents_controller.php
+++ sof/senderos/app/controllers/documents_controller.php
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 <?php
 class DocumentsController extends AppController {
 
@@ -147,3 +148,120 @@ class DocumentsController extends AppController {
 		$this->redirect(array('action' => 'index'));
 	}
 }
+=======
+<?php
+class DocumentsController extends AppController {
+
+	var $name = 'Documents';
+    var $uses = array('Document', 'Visitor');
+	function index() {
+		$this->Document->recursive = 0;
+		$this->set('documents', $this->paginate());
+	}
+
+	function view($id = null) {
+		if (!$id) {
+			$this->Session->setFlash(__('Invalid document', true));
+			$this->redirect(array('action' => 'index'));
+		}
+		$this->set('document', $this->Document->read(null, $id));
+	}
+
+    function add() {
+        //$this->layout= '';
+		if (!empty($this->data)) {
+			$this->Document->create();
+			if ($this->Document->save($this->data)) {
+                if($this->data['Document']['archivo']['error'] == 0 &&  $this->data['Document']['archivo']['size'] > 0){
+                    $nombre =  $this->data['Document']['archivo']['name'];
+                    $posicion = stripos($nombre,".");
+                    $terminacion = substr($nombre,$posicion+1);
+                    $destino = WWW_ROOT.'files'.DS;
+
+                    switch($terminacion) {
+                        case "doc": $destino = WWW_ROOT.'text'.DS; break;
+                        case "docx": $destino = WWW_ROOT.'text'.DS; break;
+                        case "txt": $destino = WWW_ROOT.'text'.DS; break;
+                        case "odt": $destino = WWW_ROOT.'text'.DS; break;
+                        case "mp3": $destino = WWW_ROOT.'sound'.DS; break;
+                        case "wav": $destino = WWW_ROOT.'sound'.DS; break;
+                        case "avi": $destino = WWW_ROOT.'sound'.DS; break;
+                        case "flv": $destino = WWW_ROOT.'video'.DS; break;
+                        case "wmv": $destino = WWW_ROOT.'video'.DS; break;
+                        case "mov": $destino = WWW_ROOT.'video'.DS; break;
+                        case "mp4": $destino = WWW_ROOT.'video'.DS; break;
+                        case "jpg": $destino = WWW_ROOT.'images'.DS; break;
+                        case "jpeg": $destino = WWW_ROOT.'images'.DS; break;
+                        case "gif": $destino = WWW_ROOT.'images'.DS; break;
+                        case "png": $destino = WWW_ROOT.'images'.DS; break;
+                        case "bmp": $destino = WWW_ROOT.'images'.DS; break;
+                        default:  $destino = WWW_ROOT.'files'.DS;
+                    }
+                    move_uploaded_file($this->data['Document']['archivo']['tmp_name'], $destino.$this->data['Document']['archivo']['name']);
+                    $this->Document->set('route', $this->data['Document']['archivo']['name']);
+                    $this->Document->save();
+                }
+				$this->Session->setFlash(__('The document has been saved'/*.$this->data['Document']['targets'][0]*/, true));
+				$this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash(__('The document could not be saved. Please, try again.', true));
+			}
+		}
+	}
+
+	function edit($id = null) {
+		if (!$id && empty($this->data)) {
+			$this->Session->setFlash(__('Invalid document', true));
+			$this->redirect(array('action' => 'index'));
+		}
+		if (!empty($this->data)) {
+            $document = $this->Document->read(null, $id);
+            $file = new File(WWW_ROOT ."/".$document['Document']['type']."/".$document['Document']['route'], false, 0777);//Si esta sirviendo esta fallando la ruta >.>
+            $this->Session->setFlash(__('The document has been saved'.WWW_ROOT ."/".$document['Document']['type']."/".$document['Document']['route'], true));
+            $file->delete();
+            $nombre =  $this->data['Document']['archivo']['name'];
+            $posicion = stripos($nombre,".");
+            $terminacion = substr($nombre,$posicion+1);
+            $destino = WWW_ROOT.'files'.DS;
+            switch($terminacion) {
+                case "doc": $destino = WWW_ROOT.'text'.DS; break;
+                case "docx": $destino = WWW_ROOT.'text'.DS; break;
+                case "txt": $destino = WWW_ROOT.'text'.DS; break;
+                case "odt": $destino = WWW_ROOT.'text'.DS; break;
+                case "mp3": $destino = WWW_ROOT.'sound'.DS; break;
+                case "wav": $destino = WWW_ROOT.'sound'.DS; break;
+                case "avi": $destino = WWW_ROOT.'sound'.DS; break;
+                case "flv": $destino = WWW_ROOT.'video'.DS; break;
+                case "wmv": $destino = WWW_ROOT.'video'.DS; break;
+                case "mov": $destino = WWW_ROOT.'video'.DS; break;
+                case "mp4": $destino = WWW_ROOT.'video'.DS; break;
+                case "jpg": $destino = WWW_ROOT.'images'.DS; break;
+                case "png": $destino = WWW_ROOT.'images'.DS; break;
+                default:  $destino = WWW_ROOT.'files'.DS;
+            }
+            move_uploaded_file($this->data['Document']['archivo']['tmp_name'], $destino.$this->data['Document']['archivo']['name']);
+            $this->Document->set('route', $this->data['Document']['archivo']['name']);
+            $this->Document->save();
+			//$this->Session->setFlash(__('The document has been saved', true));
+			$this->redirect(array('action' => 'index'));
+		}
+		if (empty($this->data)) {
+			$this->data = $this->Document->read(null, $id);
+		}
+	}
+
+	function delete($id = null) {
+		if (!$id) {
+			$this->Session->setFlash(__('Invalid id for document', true));
+			$this->redirect(array('action'=>'index'));
+		}
+            $document = $this->Document->read(null, $id);
+            $file = new File(WWW_ROOT ."/".$document['Document']['type']."/".$document['Document']['route'], false, 0777);//Si esta sirviendo esta fallando la ruta >.>
+            $file->delete();
+            $this->Document->delete($id);
+			$this->Session->setFlash(__('Document deleted', true));
+			$this->redirect(array('action'=>'index'));
+		$this->redirect(array('action' => 'index'));
+	}
+}
+>>>>>>> Stashed changes
--- sof/senderos/app/views/documents/add.ctp
+++ sof/senderos/app/views/documents/add.ctp
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 <html>
 <head>
 	<?php echo $this->Html->charset(); ?>
@@ -38,4 +39,59 @@
 	</ul>
 </div>
 </body>
+=======
+<html>
+<head>
+    <?php echo $this->Html->css('cake.generic'); ?>
+    <link rel="stylesheet" type="text/css" href="/git/sof/senderos/css/documents.css">
+</head>
+<div id="container">
+				<div id="header">
+        		    <nav id="nav">
+                        <?php echo $this->Html->link(
+                            	$this->Html->image('oet.png', array('alt' => "Inicio", 'title' => 'Inicio','style'=> "margin-left:15px;margin-top:15px;float:left;width:250px;height:75px;padding:10px;")),
+                            					array('controller'=>'pages','action' => 'home'),
+                             					array('target' => '_self', 'escape' => false)
+                            				);
+                        ?>
+                        <ul>
+                        </ul>
+                    </nav>
+               </div>
+        <div id="navegador">
+             <ul>
+                 <li><?php echo $this->Html->link(__('Estaciones', true), array('controller'=>'stations','action' => 'index')); ?></li>
+                 <li><?php echo $this->Html->link(__('Senderos', true), array('controller'=>'trails','action' => 'index')); ?></li>
+                 <li><?php echo $this->Html->link(__('Puntos', true), array('controller'=>'points','action' => 'index')); ?></li>
+             </ul>
+        </div>
+<body>
+<div id="formulario" >
+<div class="documents form">
+    <h3> Add Multimedia File </h3>
+<?php echo $this->Form->create('Document',array('type' => 'file'));?>
+	<fieldset id="formulario_interno">
+	<?php echo $this->Form->input('id', array('type' => 'hidden'));?>
+	<?php echo $this->Form->input('name');?>
+	<?php echo $this->Form->input('description');?>
+	 <?php  $types = array('sound'=>'Sound', 'video'=>'Video', 'text'=>'Text', 'images'=>'Image');
+     echo $this->Form->input('type', array('options'=>$types, 'default'=>'Sound')); ?>
+
+    <?php echo $this->Form->input('archivo', array('type' => 'file', 'label'=>'Select a file:'));?>
+    <?php echo $this->Form->input('language');?>
+    <?php echo $this->Form->input('targets', array('type' => 'select','multiple' => 'checkbox','options' => array('Student' => 'Students','Professor' => 'Professors','Scientist'=>'Scientists')));
+    ?>
+
+	</fieldset>
+<div id="submit_button"> <?php echo $this->Form->end(__('Submit', true));?> </div>
+</div>
+</div>
+<div class="actions">
+	<h3><?php __('Actions'); ?></h3>
+	<ul>
+		<li><?php echo $this->Html->link(__('List Documents', true), array('action' => 'index'));?></li>
+	</ul>
+</div>
+</body>
+>>>>>>> Stashed changes
 </html>
