b5216beaddde1b40a2fc351ddb2c85b1304506c6
erick-ostorga <ostorgach.erick@gmail.com>
2015-05-15 20:49:49 -0600

    Algunas strings de traducciones en inglés.

--- sof/senderos/app/views/layouts/en.php
+++ sof/senderos/app/views/layouts/en.php
@@ -1,5 +1,4 @@
-<?php 
-
+<?php
 $str_stations="Stations";
 $str_trails="Trails";
 $str_points="Points";
@@ -7,5 +6,22 @@ $str_documents="Documents";
 $str_clients="Clients";
 $str_login="Login";
 $str_logout="Logout";
-
+$str_location="Location";
+$str_description="Description";
+$str_name="Name";
+$str_cordx="X Coodinate";
+$str_cordy="Y Coodinate";
+$str_explore="Explore";
+$str_multimedia="Multimedia";
+$str_cancel="Cancel";
+$str_images="Images";
+$str_images_c="IMAGES";
+$str_videos="Videos";
+$str_videos_c="VIDEOS";
+$str_sounds="Sounds";
+$str_sounds_c="SOUNDS";
+$str_texts="Texts";
+$str_texts_c="TEXTS";
+$str_others="Others";
+$str_others_c="OTHERS";
 ?>
