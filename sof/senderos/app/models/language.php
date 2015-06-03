6b1de70627362fe487569dfd5a975259162cea61
erick-ostorga <ostorgach.erick@gmail.com>
2015-06-03 06:51:09 -0600

    Para arreglar versión, de nuevo.

--- sof/senderos/app/models/language.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Language extends AppModel
-{
-    var $name = 'Language';
-    var $displayField = 'name';
-    var $validate = array('code' => array('notempty' => array('rule' => array('notempty'),),),
-                          'name' => array('notempty' => array('rule' => array('notempty'),),),);
-}
