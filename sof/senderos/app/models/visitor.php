e34b4277e79420d02fc94b25f0a7e46653d16e85
alenmumo <alenmumo@gmail.com>
2015-06-06 16:27:17 -0600

    faltaba description

--- sof/senderos/app/models/visitor.php
+++ sof/senderos/app/models/visitor.php
@@ -12,7 +12,17 @@ class Visitor extends AppModel {
 				//'last' => false, // Stop validation after this rule
 				//'on' => 'create', // Limit validation to 'create' or 'update' operations
 			),
-		)
+		),
+		'description' => array(
+			/*'notempty' => array(
+				'rule' => array('notempty'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),*/
+		),
 	);
 
     public $hasMany = array('DocumentsVisitor');
