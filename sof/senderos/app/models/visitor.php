5183ce5f927f53a7e200d2969985258433d3f30f
alenmumo <alenmumo@gmail.com>
2015-06-07 21:54:10 -0600

    Update visitor.php

--- sof/senderos/app/models/visitor.php
+++ sof/senderos/app/models/visitor.php
@@ -14,16 +14,16 @@ class Visitor extends AppModel {
 			),
 		),
 		'description' => array(
-			/*'notempty' => array(
+			'notempty' => array(
 				'rule' => array('notempty'),
 				//'message' => 'Your custom message here',
 				//'allowEmpty' => false,
 				//'required' => false,
 				//'last' => false, // Stop validation after this rule
 				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),*/
+			),
 		),
 	);
 
-    public $hasMany = array('DocumentsVisitor');
+    	public $hasMany = array('DocumentsVisitor');
 }
